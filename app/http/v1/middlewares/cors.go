d1821de6dfe8610a5a1b02d3c4e1be220544bc7a
Alireza Josheghani <josheghani.dev@gmail.com>
2020-08-19 19:06:12 +0430

    Adding ACCESS_CONTROL_ALLOW_ORIGIN to .env.example file

--- app/http/v1/middlewares/cors.go
+++ app/http/v1/middlewares/cors.go
@@ -1,10 +1,13 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func CORSMiddleware(c *gin.Context) {
 
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv("ACCESS_CONTROL_ALLOW_ORIGIN"))
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "h-captcha-response, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 
