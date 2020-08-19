3eaa14cdc7f0114a6236c0874b34835cb0350b96
Alireza Josheghani <josheghani.dev@gmail.com>
2020-08-19 18:32:19 +0430

    Adding Human Recaptcha to Login Controller

--- app/http/v1/controllers/auth/create.go
+++ app/http/v1/controllers/auth/create.go
@@ -2,13 +2,12 @@ package auth
 
 import (
 	"github.com/CastyLab/api.server/app/components"
+	"github.com/CastyLab/api.server/app/components/recaptcha"
 	"github.com/CastyLab/api.server/grpc"
 	"github.com/CastyLab/grpc.proto/proto"
 	"github.com/MrJoshLab/go-respond"
 	"github.com/gin-gonic/gin"
 	"github.com/thedevsaddam/govalidator"
-	grpc2 "google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
 )
 
 // Create user jwt token
@@ -18,7 +17,6 @@ func Create(ctx *gin.Context) {
 		rules = govalidator.MapData{
 			"pass": []string{"required", "min:4", "max:50"},
 			"user": []string{"required"},
-			"g-recaptcha-response": []string{"required"},
 		}
 		opts = govalidator.Options{
 			Request:         ctx.Request,
@@ -31,25 +29,19 @@ func Create(ctx *gin.Context) {
 
 	if validate.Encode() == "" {
 
-		userInput := ctx.PostForm("user")
-
-		if userInput == "" {
+		if body, err := recaptcha.Verify(ctx); err != nil || !body.Success {
 			ctx.JSON(respond.Default.ValidationErrors(map[string] interface{} {
-				"user": []string {
-					"User field is required!",
+				"recaptcha": []string {
+					"Captcha is invalid!",
 				},
 			}))
 			return
 		}
 
-		md := metadata.New(map[string] string{
-			"g-recaptcha-response": ctx.PostForm("g-recaptcha-response"),
-		})
-
 		response, err := grpc.AuthServiceClient.Authenticate(ctx, &proto.AuthRequest{
-			User: userInput,
+			User: ctx.PostForm("user"),
 			Pass: ctx.PostForm("pass"),
-		}, grpc2.Header(&md))
+		})
 
 		code, result, ok := components.ParseGrpcErrorResponse(err)
 		if !ok {
